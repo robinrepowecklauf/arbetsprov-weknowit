d6d3a3154d6f8ff8264b4f3ae3b95b116edaf436
Robin Repo Wecklauf <robinrepowecklauf@gmail.com>
2021-04-13 00:41:29 +0200

    created RedirectButton component to switch views

--- citypop/src/views/StartView.tsx
+++ citypop/src/views/StartView.tsx
@@ -1,11 +1,13 @@
+import { RedirectButton } from '../components/RedirectButton';
+
 import '../css/start.css';
 
 export const StartView = () => {
     return (
         <div className="container">
             <div className="start-row-flexbox">
-                <button className="search-by-button">SEARCH BY CITY</button>
-                <button className="search-by-button">SEARCH BY COUNTRY</button>
+                <RedirectButton text="SEARCH BY CITY" path="/searchbycity" />
+                <RedirectButton text="SEARCH BY COUNTRY" path="/searchbycountry" />
             </div>
         </div>
     );
