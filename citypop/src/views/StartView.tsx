da04b0c4c7251ab1425facd80341c4f0ba0596af
Robin Repo Wecklauf <robinrepowecklauf@gmail.com>
2021-04-14 23:35:04 +0200

    ResultView is now implemented and works correctly

--- citypop/src/views/StartView.tsx
+++ citypop/src/views/StartView.tsx
@@ -6,8 +6,8 @@ export const StartView = () => {
     return (
         <div className="container">
             <div className="start-row-flexbox">
-                <RedirectButton text="SEARCH BY CITY" path="/search-city" />
-                <RedirectButton text="SEARCH BY COUNTRY" path="/search-country" />
+                <RedirectButton text="SEARCH BY CITY" path="/search-city" state="" />
+                <RedirectButton text="SEARCH BY COUNTRY" path="/search-country" state="" />
             </div>
         </div>
     );
